440154740e5df230559d1d8f60ddad26ce500d41
Johan Vester <johan.vester@gmail.com>
2019-01-14 11:36:28 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+class User {
+  constructor(email, name) {
+    this.email = email
+    this.name = name
+  }
+}
+
+// getUser fetches a test user with the provided id and returns a Promise that resolves into
+// an instance of the User class. Error handling is omitted.
+function getUser(id) {
+  const url = 'https://jsonplaceholder.typicode.com/users/' + id
+
+  return fetch(url)
+    // Decode from JSON
+    .then(response => response.json())
+
+    // Marshal JSON into our own User class
+    .then(json => new Promise((resolve, reject) => {
+      resolve(new User(json.email, json.name))
+    }))
+}
+
+// renderUser populates the name and email elements from the provided User object.
+function renderUser(user) {
+  const emailElement = document.getElementById('email')
+  const nameElement = document.getElementById('name')
+
+  emailElement.innerHTML = user.email
+  nameElement.innerHTML = user.name
+}
+
+function randomNumberBetween(min, max) {
+  return min + Math.floor(Math.random() * max)
+}
+
+// Set up a click event listener on the fetch button.
+document.getElementById('fetch').onclick = () => {
+  // We know there are exactly 10 test users, so we'll choose one randomly.
+  const userId = randomNumberBetween(1, 10)
+
+  getUser(userId).then(renderUser)
+}
